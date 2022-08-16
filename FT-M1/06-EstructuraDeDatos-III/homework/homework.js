ebc76b08ddd0381ac2d864a85cc91bd82411383a
Franco Borghi <francox93@hotmail.com>
2022-08-16 18:20:21 -0300

    Doing homework data structure 3

--- FT-M1/06-EstructuraDeDatos-III/homework/homework.js
+++ FT-M1/06-EstructuraDeDatos-III/homework/homework.js
@@ -11,7 +11,77 @@
   El ábrol utilizado para hacer los tests se encuentra representado en la imagen bst.png dentro del directorio homework.
 */
 
-function BinarySearchTree() {}
+class BinarySearchTree {
+
+  constructor(value) {
+    this.value = value;
+    this.right = null;
+    this.left = null;
+    this.length = 1;
+  }
+
+  size() {
+    return this.length;
+  }
+
+  insert(value) {
+
+    this.length++;
+
+    if(this.value > value) {
+      if(this.left === null) {
+        this.left =  new BinarySearchTree(value);
+      } else {
+        this.left.insert(value);
+      }
+    } else {
+      if(this.right === null) {
+        this.right = new BinarySearchTree(value);
+      } else {
+        this.right.insert(value);
+      }
+    }
+  }
+
+  
+  contains(value) {
+      
+    if(this.value === value){
+      return true;
+    }
+    if(value < this.value && this.left !== null){
+      return this.left.contains(value);
+    }else if(value > this.value && this.right !== null){
+      return this.right.contains(value)
+    }else{
+      return false;
+    }
+
+  }
+
+  height(){ 
+    if(!this.left && !this.right){
+      return 0;
+    }else{
+      if(this.left && !this.right){
+        return this.left.height() + 1;
+      }else if(!this.left && this.right){
+        return this.right.height() + 1;
+      }else{
+        return max(this.left.height() + 1, this.right.height() + 1 );
+      }
+    }
+  }
+
+  depthFirstForEach() {
+
+  }
+
+  breadthFirstForEach() {
+
+  }
+
+}
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
