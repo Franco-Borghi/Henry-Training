66baa8420084dbc7a6d9ed61df4a14c13d514798
Franco Borghi <francox93@hotmail.com>
2022-08-25 01:08:31 -0300

    Doing homework 07

--- FT-M1/07-Algoritmos-I/homework/homework.js
+++ FT-M1/07-Algoritmos-I/homework/homework.js
@@ -7,12 +7,32 @@ function factorear(num) {
   // Ej: factorear(180) --> [1, 2, 2, 3, 3, 5] Ya que 1x2x2x3x3x5 = 180 y son todos números primos
   // Tu código:
   let factorArray = [1];
-  let finish = 0;
 
-  
-  
+  while (num > 1) {
+    if (num%2 === 0) {
+      factorArray.push(2);
+      num = num/2;
+    } else if (num%3 === 0) {
+      factorArray.push(3); 
+      num = num/3;
+    } else if (num%5 === 0) {
+      factorArray.push(5); 
+      num = num/5;
+    } else if (num%7 === 0) {
+      factorArray.push(7); 
+      num = num/7;
+    }  else {
+      factorArray.push(num);
+      num = 1;
+    }
+  }
+
+  factorArray.sort(function(a, b) {
+    return a - b;
+  });
 
   return factorArray;
+
 }
 
 function bubbleSort(array) {
@@ -21,7 +41,25 @@ function bubbleSort(array) {
   // Devolver el array ordenado resultante
   // Tu código:
 
+  let finish = 1;
+
+  for (let i = 0; i < (array.length - 1); i++) {
+    if (array[i] > array[i+1]) {
+      let min = array[i+1];
+      let max = array[i];
+      array[i] = min
+      array[i+1] = max;
+      finish = 0;
+    }
+  }
+
+  if (finish == 1) {
+    return array
+  } else {
+    return bubbleSort(array);
+  }
 }
+  
 
 
 function insertionSort(array) {
@@ -29,7 +67,19 @@ function insertionSort(array) {
   // el array recibido como parámetro utilizando arreglos
   // Devolver el array ordenado resultante
   // Tu código:
+  for (let i = 1; i < array.length; i++){
 
+    let current = array[i];
+    let comparer = i-1;
+    
+    while (comparer >= 0 && array[comparer] > current) {
+      array[comparer+1] = array[comparer];
+      comparer--;
+    }
+    array[comparer+1] = current;
+
+  }
+  return array;
 }
 
 
@@ -51,4 +101,3 @@ module.exports = {
   insertionSort,
   selectionSort,
 };
->>>>>>> .merge_file_Dg4clG
