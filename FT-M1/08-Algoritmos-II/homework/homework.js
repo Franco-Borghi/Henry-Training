f97e36d6acd2509182a2bd1d3dfb45a972f4d643
Franco Borghi <francox93@hotmail.com>
2022-08-26 15:44:39 -0300

    Homework 08 QuickSort Done

--- FT-M1/08-Algoritmos-II/homework/homework.js
+++ FT-M1/08-Algoritmos-II/homework/homework.js
@@ -7,6 +7,38 @@ function quickSort(array) {
   // Devolver el array ordenado resultante
   // Tu código:
 
+
+
+  let indexOfPivot = Math.floor(Math.random()*array.length); // Calculo el indice del pivote con Math.random
+
+  let pivot = [array[indexOfPivot]]; // Declaro el pivote y se asigno el valor que se encuentra en el indice calculado
+
+  array.splice(indexOfPivot, 1); // Elimino el pivote del array padre
+
+  let minArray = []; 
+  let maxArray = [];
+
+  if (array.length > 0) { // Envio elementos mayores a pivote a maxArray y menores a pivote a minArray
+    array.forEach(element => {
+      if (element  < pivot) {
+        minArray.push(element);
+      }
+
+      if (element >= pivot) {
+        maxArray.push(element);
+      }
+    });
+  }
+
+  if (minArray.length > 1) { // Si minArray contiene mas de 1 elemento aplico recursion
+    minArray = quickSort(minArray);
+  }
+
+  if (maxArray.length > 1) { // Si maxArray contiene mas de 1 elemento aplico recursion
+    maxArray = quickSort(maxArray);
+  }
+  
+  return minArray.concat(pivot.concat(maxArray)); // Retorno los 3 arrays concatenados en orden
 }
 
 function mergeSort(array) {
@@ -15,6 +47,27 @@ function mergeSort(array) {
   // Devolver el array ordenado resultante
   // Tu código:
 
+  let leftArray;
+
+  let rightArray;
+
+  let returnArray;
+
+  if (array.length > 1) {
+    leftArray = array.slice(0, Math.floor(array.length / 2));
+    rightArray = array.slice(Math.floor(array.length / 2));
+  }
+
+  if (leftArray > 1) {
+    leftArray = mergeSort(leftArray);
+  }
+
+  if (rightArray > 1) {
+    rightArray = mergeSort(rightArray);
+  }
+
+  
+  
 }
 
 // No modificar nada debajo de esta línea
